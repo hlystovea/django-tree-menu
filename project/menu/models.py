261e3049495e993a1ba5e945f4bfe0a6aea77eeb
Evgenii Khlystov <hlystovea@gmail.com>
2023-01-02 19:38:50 +0700

    Create model Menu

--- project/menu/models.py
+++ project/menu/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(
+        verbose_name=_('Имя'),
+        max_length=100,
+        unique=True
+    )
+    slug = models.SlugField(
+        verbose_name=_('Слаг'),
+        unique=True
+    )
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = _('Меню')
+        verbose_name_plural = _('Меню')
+
+    def __str__(self):
+        return self.name

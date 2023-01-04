80d39f53a5266d4b04dbfd256e348867775c3586
Evgenii Khlystov <hlystovea@gmail.com>
2023-01-04 22:54:18 +0700

    Remove constraint slug_menu_uniquetogether

--- project/menu/models.py
+++ project/menu/models.py
@@ -28,7 +28,8 @@ class Item(models.Model):
         max_length=100
     )
     slug = models.SlugField(
-        verbose_name=_('Слаг')
+        verbose_name=_('Слаг'),
+        unique=True
     )
     menu = models.ForeignKey(
         Menu,
@@ -49,12 +50,6 @@ class Item(models.Model):
         ordering = ('name', )
         verbose_name = _('Пункт меню')
         verbose_name_plural = _('Пункты меню')
-        constraints = [
-            models.UniqueConstraint(
-                fields=('slug', 'menu'),
-                name='slug_menu_uniquetogether'
-            )
-        ]
 
     def __str__(self):
         return f'{self.menu}: {self.name}'
